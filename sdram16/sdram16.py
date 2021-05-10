781d5c24aa2c5816a5ded808c1dc27b2a15f7e06
lawrie <lawrie.griffiths@ntlword.com>
2021-05-10 19:52:22 +0100

    16-bit SDRAM controller (not working)

---
+++ sdram16/sdram16.py
@@ -0,0 +1,163 @@
+from nmigen import *
+
+# SDRAM controller with 16-bit reads and writes
+class Sdram(Elaboratable):
+    def __init__(self):
+
+        # Chip interface
+        self.sd_data_in  = Signal(16)
+        self.sd_data_out = Signal(16)
+        self.sd_data_dir = Signal()
+        self.sd_addr     = Signal(11)
+        self.sd_dqm      = Signal(2)
+        self.sd_ba       = Signal(1)
+        self.sd_cs       = Signal()
+        self.sd_we       = Signal()
+        self.sd_ras      = Signal()
+        self.sd_cas      = Signal()
+
+        # Control
+        self.init        = Signal()
+        self.sync        = Signal()
+
+        # Port
+        self.din         = Signal(16)
+        self.dout        = Signal(16)
+        self.addr        = Signal(20) # Word address
+        self.ds          = Signal(2)
+        self.oe          = Signal()
+        self.we          = Signal()
+
+    def elaborate(self, platform):
+
+        m = Module()
+
+        # Configure SDRAM access
+        RASCAS_DELAY   = C(2,3)
+        BURST_LENGTH   = C(0,3)
+        ACCESS_TYPE    = C(0,1)
+        CAS_LATENCY    = C(2,3)
+        OP_MODE        = C(0,2)
+        NO_WRITE_BURST = C(1,1)
+
+        MODE = Cat([BURST_LENGTH, ACCESS_TYPE, CAS_LATENCY, OP_MODE, NO_WRITE_BURST, C(0,1)])
+
+        # States
+        STATE_FIRST     = C(0,3)
+        STATE_CMD_START = C(1,3)
+        STATE_CMD_CONT  = STATE_CMD_START + RASCAS_DELAY
+        STATE_READ      = STATE_CMD_CONT + CAS_LATENCY + C(1,3)
+        STATE_HIGHZ     = STATE_READ - C(1,3)
+
+        # Reset counts down after init set
+        reset = Signal(5)
+        stage = Signal(3)
+
+        with m.If(self.init):
+            m.d.sdram += reset.eq(C(0x1f,5))
+        with m.Elif((stage == STATE_FIRST) & (reset != 0)):
+            m.d.sdram += reset.eq(reset-1)
+
+        # SDRAM commands
+        CMD_INHIBIT          = C(0b1111,4)
+        CMD_NOP              = C(0b0111,4)
+        CMD_ACTIVE           = C(0b0011,4)
+        CMD_READ             = C(0b0101,4)
+        CMD_WRITE            = C(0b0100,4)
+        CMD_BURST_TERMINATE  = C(0b0110,4)
+        CMD_PRECHARGE        = C(0b0010,4)
+        CMD_AUTO_REFRESH     = C(0b0001,4)
+        CMD_LOAD_MODE        = C(0b0000,4)
+
+        # Drive control signals from current command
+        sd_cmd   = Signal(4)
+
+        m.d.comb += [
+            self.sd_cs.eq(sd_cmd[3]),
+            self.sd_ras.eq(sd_cmd[2]),
+            self.sd_cas.eq(sd_cmd[1]),
+            self.sd_we.eq(sd_cmd[0])
+        ]
+
+        mode     = Signal(2)
+        din_r    = Signal(16)
+
+        m.d.comb += [
+            self.sd_data_out.eq(din_r),
+            self.sd_data_dir.eq(mode[1]),
+        ]
+
+        addr_r   = Signal(11)
+        ds_r     = Signal(2)
+        old_sync = Signal()
+
+        with m.If(stage.any()):
+            m.d.sdram += stage.eq(stage+1)
+
+        m.d.sdram += [
+            old_sync.eq(self.sync),
+            sd_cmd.eq(CMD_INHIBIT)
+        ]
+
+        with m.If(~old_sync & self.sync):
+            m.d.sdram += stage.eq(1)
+
+        with m.If(reset != 0):
+            with m.If(stage == STATE_CMD_START):
+                with m.If(reset == 13):
+                    m.d.sdram += [
+                        sd_cmd.eq(CMD_PRECHARGE),
+                        self.sd_addr[10].eq(1)
+                    ]
+                with m.If(reset == 2):
+                    m.d.sdram += [
+                        sd_cmd.eq(CMD_LOAD_MODE),
+                        self.sd_addr.eq(MODE)
+                    ]
+            m.d.sdram += [
+                mode.eq(0),
+                self.sd_dqm.eq(C(0b11,2))
+            ]
+        with m.Else():
+            # Normal operation
+            with m.If(stage == STATE_CMD_START):
+                with m.If(self.we | self.oe):
+                    # RAS phase
+                    m.d.sdram += [
+                        mode.eq(Cat(self.oe, self.we)),
+                        sd_cmd.eq(CMD_ACTIVE),
+                        self.sd_addr.eq(self.addr[8:19]),
+                        self.sd_ba.eq(self.addr[19]),
+                        ds_r.eq(self.ds),
+                        din_r.eq(self.din),
+                        addr_r.eq(Cat(self.addr[:8],C(0b100,3)))
+                    ]
+                with m.Else():
+                    m.d.sdram += [
+                        sd_cmd.eq(CMD_AUTO_REFRESH),
+                        mode.eq(0)
+                    ]
+
+                # CAS phase
+                with m.If((stage == STATE_CMD_CONT) & (mode != 0)):
+                    m.d.sdram += [
+                        sd_cmd.eq(Mux(mode[1], CMD_WRITE, CMD_READ)),
+                        self.sd_addr.eq(addr_r)
+                    ]
+
+                    with m.If(mode[1]):
+                        m.d.sdram += self.sd_dqm.eq(~ds_r)
+                    with m.Else():
+                        m.d.sdram += self.sd_dqm.eq(C(0b00,2))
+
+                with m.If(stage == STATE_HIGHZ):
+                    m.d.sdram += [
+                        self.sd_dqm.eq(C(0b11,2)),
+                        mode[1].eq(0)
+                    ]
+
+                with m.If((stage == STATE_READ) & (mode != 0)):
+                    m.d.sdram += self.dout.eq(self.sd_data_in)
+
+        return m
+
---
+++ sdram16/test_sdram16.py
@@ -0,0 +1,82 @@
+from nmigen import *
+from debouncer import Debouncer
+from sdram_controller16 import sdram_controller
+from pll import PLL
+
+class Top(Elaboratable):
+    def elaborate(self, platform):
+        m = Module()
+
+        led = [platform.request("led",count) for count in range(4)]
+        leds = Cat([i.o for i in led])
+        clk_in = platform.request(platform.default_clk, dir='-')[0]
+
+        # Clock generation
+        # PLL - 64MHz for sdram
+        m.submodules.pll = pll = PLL(freq_in_mhz=25, freq_out_mhz=64, domain_name="sdram")
+
+        m.domains.sdram = cd_sdram = pll.domain
+        m.d.comb += pll.clk_pin.eq(clk_in)
+
+        # Divide clock by 8
+        div = Signal(3)
+        m.d.sdram += div.eq(div+1)
+
+        # Make sync domain 8MHz
+        m.domains.sync = cd_sync = ClockDomain("sync")
+        m.d.comb += ClockSignal().eq(div[2])
+
+        reset = Signal(10, reset=0)
+        with m.If(~reset.all()):
+            m.d.sync += reset.eq(reset+1)
+
+        # Add the SDRAM controller
+        m.submodules.mem = mem = sdram_controller()
+
+        addr = Signal(20) # word address
+        din = Signal(16)   # data to be written to SDRAM
+
+        m.d.comb += [
+            mem.init.eq(~reset.all()),
+            mem.address.eq(addr),
+            mem.data_in.eq(din),
+            mem.sync.eq(div[2])
+        ]
+
+        # Writes and reads
+        cnt = Signal(20)
+
+        m.d.sync += [
+            cnt.eq(cnt + 1),
+            mem.req_write.eq(0),
+            mem.req_read.eq(0)
+        ]
+
+        with m.If(cnt == 100):
+            m.d.sync += [
+                mem.req_write.eq(1),
+                din.eq(0x0102),
+                addr.eq(100)
+            ]
+        with m.If(cnt == 200):
+            m.d.sync += [
+                mem.req_write.eq(1),
+                din.eq(0x0304),
+                addr.eq(101)
+            ]
+        with m.If(cnt == 300):
+            m.d.sync += [
+                mem.req_read.eq(1),
+                addr.eq(101)
+            ]
+
+        # Show 3 bits of data from SDRAM on the leds
+        m.d.comb += leds.eq(mem.data_out)
+
+        return m
+
+if __name__ == "__main__":
+    from nmigen_boards.blackice_mx import BlackIceMXPlatform
+    platform = BlackIceMXPlatform()
+    platform.build(Top(), do_program=True)
+
