3efa1ddf0ad209e0247020a22ac60e1a8f123f16
lawrie <lawrie.griffiths@ntlword.com>
2021-05-11 11:14:50 +0100

    Improved sdram16 test

--- sdram16/test_sdram16.py
+++ sdram16/test_sdram16.py
@@ -1,14 +1,31 @@
 from nmigen import *
-from debouncer import Debouncer
+from nmigen.build import *
 from sdram_controller16 import sdram_controller
 from pll import PLL
 
+# Diglient 8-LED Pmods for diagnostics 
+leds8_1_pmod = [
+    Resource("leds8_1", 0,
+            Subsignal("leds", Pins("7 8 9 10 1 2 3 4", dir="o", conn=("pmod",2)), Attrs(IO_STANDARD="SB_LVCMOS")))
+]
+
+leds8_2_pmod = [
+    Resource("leds8_2", 0,
+            Subsignal("leds", Pins("7 8 9 10 1 2 3 4", dir="o", conn=("pmod",3)), Attrs(IO_STANDARD="SB_LVCMOS")))
+]
+
+# Test of 16-bit SDRAM controller
 class Top(Elaboratable):
     def elaborate(self, platform):
         m = Module()
 
+        # Get pins
         led = [platform.request("led",count) for count in range(4)]
         leds = Cat([i.o for i in led])
+        leds8_1 = platform.request("leds8_1")
+        leds8_2 = platform.request("leds8_2")
+        led16 =  [i for i in leds8_1] + [i for i in leds8_2]
+        leds16 = Cat([i for i in led16])
         clk_in = platform.request(platform.default_clk, dir='-')[0]
 
         # Clock generation
@@ -26,6 +43,7 @@ class Top(Elaboratable):
         m.domains.sync = cd_sync = ClockDomain("sync")
         m.d.comb += ClockSignal().eq(div[2])
 
+        # Power-on reset (not used)
         reset = Signal(10, reset=0)
         with m.If(~reset.all()):
             m.d.sync += reset.eq(reset+1)
@@ -33,50 +51,67 @@ class Top(Elaboratable):
         # Add the SDRAM controller
         m.submodules.mem = mem = sdram_controller()
 
-        addr = Signal(20) # word address
-        din = Signal(16)   # data to be written to SDRAM
+        # RAM test
+        addr   = Signal(20, reset=0) # word address
+        din    = Signal(16)          # data to be written to SDRAM
+        count  = Signal(5,  reset=0) # width control speed of read back
+        we     = Signal(1,  reset=0) # request write
+        oe     = Signal(1,  reset=0) # request read
+        read   = Signal(1,  reset=0) # Set for read back phase
+        err    = Signal(1,  reset=0) # Set when error is detected
+        passed = Signal(1,  reset=0) # Set if test passed
 
         m.d.comb += [
-            mem.init.eq(~reset.all()),
+            mem.init.eq(~pll.locked), # Use pll not locked as signal to initialise SDRAM 
+            mem.sync.eq(div[2]),      # Sync with 8MHz clock
             mem.address.eq(addr),
             mem.data_in.eq(din),
-            mem.sync.eq(div[2])
+            mem.req_read.eq(oe),
+            mem.req_write.eq(we),
+            din.eq(addr[4:]),         # Write most significant 16 bits of address
+            leds16.eq(mem.data_out)   # Put the data read on the debugg leds
         ]
 
-        # Writes and reads
-        cnt = Signal(20)
+        # Set the error flag if read gives the wrong value
+        with m.If((count > 0) & read & (mem.data_out != addr[4:])):
+            m.d.sync += err.eq(1)
 
+        # Increment count and do transfer when count is 0
         m.d.sync += [
-            cnt.eq(cnt + 1),
-            mem.req_write.eq(0),
-            mem.req_read.eq(0)
+            count.eq(count+1),
+            we.eq((count == 0) & ~read),
+            oe.eq((count == 0) & read)
         ]
 
-        with m.If(cnt == 100):
-            m.d.sync += [
-                mem.req_write.eq(1),
-                din.eq(0x0102),
-                addr.eq(100)
-            ]
-        with m.If(cnt == 200):
-            m.d.sync += [
-                mem.req_write.eq(1),
-                din.eq(0x0304),
-                addr.eq(101)
-            ]
-        with m.If(cnt == 300):
-            m.d.sync += [
-                mem.req_read.eq(1),
-                addr.eq(100)
-            ]
+        # Increment address every other cycle for write or when
+        # count is exhausted for reads
+        with m.If((~read & (count == 1)) | count.all()):
+            with m.If(~read & (count == 1)):
+                m.d.sync += count.eq(0)
 
-        # Show 3 bits of data from SDRAM on the leds
-        m.d.comb += leds.eq(mem.data_out)
+            m.d.sync += addr.eq(addr+1)
+
+            with m.If(addr.all()):
+                # Switch to read when all data is written
+                m.d.sync += read.eq(1)
+
+                # Set passed flag when all data has been read without an error
+                with m.If(read & ~err):
+                    m.d.sync += passed.eq(1)
+
+        # Show flags on the leds
+        # Blue led on during write phase, green led means passed, red means error
+        m.d.comb += leds.eq(Cat([~read, passed, C(0,1), err]))
 
         return m
 
 if __name__ == "__main__":
     from nmigen_boards.blackice_mx import BlackIceMXPlatform
+
     platform = BlackIceMXPlatform()
+
+    platform.add_resources(leds8_1_pmod)
+    platform.add_resources(leds8_2_pmod)
+
     platform.build(Top(), do_program=True)
 
