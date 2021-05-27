b00e8784269d7c8606166897d99db023aca68464
lawrie <lawrie.griffiths@ntlword.com>
2021-05-27 19:59:43 +0100

    Improved code and comments

--- mitecpu/mitecpu.py
+++ mitecpu/mitecpu.py
@@ -1,57 +1,45 @@
 from nmigen import *
 from nmigen_boards.blackice_mx import *
 
-def readhex():
-    f = open("progmem.hex","r")
-    l = []
-    while True:
-        s = f.readline()
-        if s:
-            l.append(int(s,16))
-        else:
-            break
-    f.close()
-    return l
+from readhex import readhex
 
+# Tiny CPU converted from Verilog - https://github.com/jbush001/MiteCPU
 class MiteCPU(Elaboratable):
     def elaborate(self, platform):
-        led   = [platform.request("led", i) for i in range(4)]
-        clk25 = platform.request("clk25")
+        leds   = Cat([platform.request("led", i) for i in range(4)])
+
+        # Read in program and print it in hex
         prog = readhex()
         print(" ".join(hex(n) for n in prog))
+
+        # Code and data storage
         code = Memory(width=11, depth=256, init=prog)
         data = Memory(width=8, depth=256)
-        ip = Signal(8, reset=0xff)
-        ip_nxt = Signal(8)
-        instr = Signal(11, reset=0)
-        acc = Signal(8, reset=0)
-        index = Signal(8, reset=0)
-        op = Signal(8, reset=0)
-        addr = Signal(8)
-        leds = Cat(led[i].o for i in range(4))
-        delay = Signal(22)
+
+        # Registers and other signals
+        ip     = Signal(8,  reset=0xff) # Instruction pointer
+        ip_nxt = Signal(8)              # Next instruction pointer
+        instr  = Signal(11, reset=0)    # Current instruction
+        acc    = Signal(8,  reset=0)    # Accumulator
+        index  = Signal(8,  reset=0)    # Index register
+        op     = Signal(8,  reset=0)    # The operand
+        addr   = Signal(8)              # Address
+        delay  = Signal(22)             # Delay counter
 
         m = Module()
-
-        # Create sync domain
-        m.domains.sync = ClockDomain()
-        m.d.comb += ClockSignal().eq(clk25.i)
 
         # Create negedge domain
         neg = ClockDomain("neg", clk_edge="neg")
         m.domains += neg
-        m.d.comb += neg.clk.eq(clk25.i)
+        m.d.comb += neg.clk.eq(ClockSignal())
        
         # Set address 
-        m.d.comb += addr.eq(instr[0:8] + index)
+        m.d.comb += addr.eq(instr[:8] + index)
 
         # Set ip_nxt
-        with m.If((instr[8:] == 4) & acc[7]):
-            m.d.comb += ip_nxt.eq(instr[0:8])
-        with m.Else():
-            m.d.comb += ip_nxt.eq(ip + 1)
+        m.d.comb += ip_nxt.eq(Mux((instr[8:] == 4) & acc[7], instr[:8], ip + 1))
 
-        # Delay counter
+        # Delay counter to execute slowly and see result on leds
         m.d.sync += delay.eq(delay + 1)
 
         with m.If(delay == 0):
@@ -71,20 +59,21 @@ class MiteCPU(Elaboratable):
                 with m.Case("110"):
                     m.d.sync += acc.eq(acc & op)
                 with m.Case("010"):
-                    m.d.sync += acc.eq(instr[0:8])
+                    m.d.sync += acc.eq(instr[:8])
                 with m.Case("101"):
                     m.d.sync += index.eq(op)
                 with m.Case("011"):
                     m.d.sync += data[addr].eq(acc)
                     # data[0] is leds
-                    with m.If(instr[0:8] == 0):
+                    with m.If(instr[:8] == 0):
                         m.d.sync += leds.eq(acc)
            
-            # Fetch the op on the negative edge of clock
+            # Fetch the operand on the negative edge of clock
             m.d.neg += op.eq(data[addr])
         
         return m
 
 if __name__ == "__main__":
     platform = BlackIceMXPlatform()
-    platform.build(MiteCPU(), do_program=True)
+    platform.build(MiteCPU(), nextpnr_opts="--timing-allow-fail", do_program=True)
+
--- mitecpu/readhex.py
+++ mitecpu/readhex.py
@@ -1,11 +1,12 @@
-f = open("progmem.hex","r")
-l = []
-while True:
-  s = f.readline()
-  if s:
-    l.append(int(s,16))
-  else:
-    break
-print(l)
-f.close()
+def readhex():
+    f = open("progmem.hex","r")
+    l = []
+    while True:
+        s = f.readline()
+        if s:
+            l.append(int(s,16))
+        else:
+            break
+    f.close()
+    return l
 
