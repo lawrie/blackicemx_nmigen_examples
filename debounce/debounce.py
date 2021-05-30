18d388172288651d0da1c29522e04f51b2fe6f12
lawrie <lawrie.griffiths@ntlword.com>
2021-05-30 20:01:17 +0100

    Minor changes

--- debounce/debounce.py
+++ debounce/debounce.py
@@ -1,18 +1,16 @@
 from nmigen import *
 from nmigen_boards.blackice_mx import *
 
-from debouncer import *
+from debouncer import Debouncer
 
 class Debounce(Elaboratable):
     def elaborate(self, platform):
-        led  = [platform.request("led", i) for i in range(1,4)]
+        leds  = Cat([platform.request("led", i) for i in range(1,4)])
         btn1 = platform.request("button", 0)
-        leds = Cat([led[i].o for i in range(3)])
 
         m = Module()
 
-        debouncer = Debouncer()
-        m.submodules.debouncer = debouncer
+        m.submodules.debouncer = debouncer = Debouncer()
 
         m.d.comb += debouncer.btn.eq(btn1)
 
@@ -20,7 +18,6 @@ class Debounce(Elaboratable):
             m.d.sync += leds.eq(leds+1)
 
         return m
-
 
 if __name__ == "__main__":
     platform = BlackIceMXPlatform()
