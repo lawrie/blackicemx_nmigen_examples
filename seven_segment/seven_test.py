df56bbf49d5b22083bcecd5946638e81ffcf9068
lawrie <lawrie.griffiths@ntlword.com>
2021-05-31 08:24:56 +0100

    Imprioved 7-segment test

--- seven_segment/seven_test.py
+++ seven_segment/seven_test.py
@@ -5,35 +5,40 @@ from seven_seg import SevenSegController
 
 seven_seg_pmod = [
     Resource("seven_seg", 0,
-            Subsignal("aa", Pins("7", dir="o", conn=("pmod",2)), Attrs(IO_STANDARD="SB_LVCMOS")),
-            Subsignal("ab", Pins("8", dir="o", conn=("pmod",2)), Attrs(IO_STANDARD="SB_LVCMOS")),
-            Subsignal("ac", Pins("9", dir="o", conn=("pmod",2)), Attrs(IO_STANDARD="SB_LVCMOS")),
+            Subsignal("aa", Pins("7",  dir="o", conn=("pmod",2)), Attrs(IO_STANDARD="SB_LVCMOS")),
+            Subsignal("ab", Pins("8",  dir="o", conn=("pmod",2)), Attrs(IO_STANDARD="SB_LVCMOS")),
+            Subsignal("ac", Pins("9",  dir="o", conn=("pmod",2)), Attrs(IO_STANDARD="SB_LVCMOS")),
             Subsignal("ad", Pins("10", dir="o", conn=("pmod",2)), Attrs(IO_STANDARD="SB_LVCMOS")),
-            Subsignal("ae", Pins("7", dir="o", conn=("pmod",3)), Attrs(IO_STANDARD="SB_LVCMOS")),
-            Subsignal("af", Pins("8", dir="o", conn=("pmod",3)), Attrs(IO_STANDARD="SB_LVCMOS")),
-            Subsignal("ag", Pins("9", dir="o", conn=("pmod",3)), Attrs(IO_STANDARD="SB_LVCMOS")),
+            Subsignal("ae", Pins("7",  dir="o", conn=("pmod",3)), Attrs(IO_STANDARD="SB_LVCMOS")),
+            Subsignal("af", Pins("8",  dir="o", conn=("pmod",3)), Attrs(IO_STANDARD="SB_LVCMOS")),
+            Subsignal("ag", Pins("9",  dir="o", conn=("pmod",3)), Attrs(IO_STANDARD="SB_LVCMOS")),
             Subsignal("ca", Pins("10", dir="o", conn=("pmod",3)), Attrs(IO_STANDARD="SB_LVCMOS")))
 ]
 
 class SevenTest(Elaboratable):
     def elaborate(self, platform):
-        led = [platform.request("led", i) for i in range(4)]
+        # Get pins
         seg_pins = platform.request("seven_seg")
+        leds7 = Cat([seg_pins.aa, seg_pins.ab, seg_pins.ac, seg_pins.ad,
+                     seg_pins.ae, seg_pins.af, seg_pins.ag])
 
-        timer = Signal(28)
-        seven = SevenSegController()
-
+        # Add 7-segment controller
         m = Module()
-        m.submodules.seven = seven
+        m.submodules.seven = seven = SevenSegController()
+
+        # Timer
+        timer = Signal(32)
         m.d.sync += timer.eq(timer + 1)
+
+        # Connect pins 
         m.d.comb += [
-            Cat([i.o for i in led]).eq(timer[-9:-1]),
-            Cat([seg_pins.aa, seg_pins.ab, seg_pins.ac, seg_pins.ad,
-                 seg_pins.ae, seg_pins.af, seg_pins.ag]).eq(seven.leds),
-            seg_pins.ca.eq(timer[-1])
+            leds7.eq(seven.leds),
+            # Each digit refreshed at 100Hz
+            seg_pins.ca.eq(timer[18])
         ]
-             
-        m.d.comb += seven.val.eq(timer[-5:-1])
+    
+        # Set digits to 4-bit slices of timer, to count up
+        m.d.comb += seven.val.eq(Mux(timer[18], timer[-5:-1], timer[-9:-5]))
 
         return m
 
@@ -41,3 +46,4 @@ if __name__ == "__main__":
     platform = BlackIceMXPlatform()
     platform.add_resources(seven_seg_pmod)
     platform.build(SevenTest(), do_program=True)
+
