8006c7d5cb0ff5d668cf99082c9a48ab4c2496c4
lawrie <lawrie.griffiths@ntlword.com>
2021-06-08 11:37:40 +0100

    Connected memory and DSPI pins

--- spimem/test_spi.py
+++ spimem/test_spi.py
@@ -5,20 +5,24 @@ from spimem import SpiMem
 
 class Top(Elaboratable):
     def elaborate(self, platform):
+        csn = platform.request("dcs")
+        sclk = platform.request("dsck")
+        copi = platform.request("dd0")
+        cipo = platform.request("dd1", dir="-")
 
         m = Module()
 
-        csn = Signal()
-        copi = Signal()
-        cipo = Signal()
-        sclk = Signal()
         rd   = Signal()
         wr   = Signal()
         addr = Signal(32)
         din  = Signal(8)
         dout = Signal(8)
 
-        m.submodules.spimem = spimem = SpiMem(addr_bits=32)
+        m.submodules.spimem = spimem = SpiMem(addr_bits=16)
+
+        mem = Memory(width=8, depth=4 * 1024)
+        m.submodules.r = r = mem.read_port()
+        m.submodules.w = w = mem.write_port()
 
         m.d.comb += [
             spimem.csn.eq(csn),
@@ -29,7 +33,12 @@ class Top(Elaboratable):
             addr.eq(spimem.addr),
             dout.eq(spimem.dout),
             rd.eq(spimem.rd),
-            wr.eq(spimem.wr)
+            wr.eq(spimem.wr),
+            r.addr.eq(addr),
+            din.eq(r.data),
+            w.data.eq(dout),
+            w.addr.eq(addr),
+            w.en.eq(wr)
         ]
 
         return m
