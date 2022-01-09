324b41000118ae841720559f97f4c99f35f548f5
lawrie <lawrie.griffiths@ntlword.com>
2022-01-09 17:08:03 +0000

    Removed redundant code and support for 8-pin LCD with csn connected

--- st7789/st7789_test.py
+++ st7789/st7789_test.py
@@ -15,9 +15,9 @@ oled_pmod = [
 
 class ST7789Test(Elaboratable):
     def elaborate(self, platform):
-        led = [platform.request("led", i) for i in range(4)]
+        #led = [platform.request("led", i) for i in range(4)]
 
-        # OLED
+        # LCD/OLED Pmod
         oled  = platform.request("oled")
         oled_clk  = oled.oled_clk
         oled_mosi = oled.oled_mosi
@@ -25,42 +25,38 @@ class ST7789Test(Elaboratable):
         oled_resn = oled.oled_resn
         oled_csn  = oled.oled_csn
 
-        st7789 = ST7789(150000)
+        st7789 = ST7789(reset_delay=150000)
         m = Module()
         m.submodules.st7789 = st7789
        
         x = Signal(8)
         y = Signal(8)
-        next_pixel = Signal()
+        #next_pixel = Signal()
  
         m.d.comb += [
             oled_clk .eq(st7789.spi_clk),
             oled_mosi.eq(st7789.spi_mosi),
             oled_dc  .eq(st7789.spi_dc),
             oled_resn.eq(st7789.spi_resn),
-            oled_csn .eq(1),
-            next_pixel.eq(st7789.next_pixel),
+            oled_csn .eq(st7789.spi_csn),
+            #oled_csn .eq(1), # For 7-pin version that backlight instead of csn
+            #next_pixel.eq(st7789.next_pixel),
             x.eq(st7789.x),
             y.eq(st7789.y),
         ]
 
+        # Draw chequered pattern
         with m.If(x[4] ^ y[4]):
             m.d.comb += st7789.color.eq(x[3:8] << 6)
         with m.Else():
             m.d.comb += st7789.color.eq(y[3:8] << 11)
 
-
-        m.d.comb += [
-            Cat([i.o for i in led]).eq(st7789.x)
-        ]
+        #m.d.comb += Cat([i.o for i in led]).eq(st7789.x)
 
         return m
 
 if __name__ == "__main__":
     platform = BlackIceMXPlatform()
-    
-    # Add the OLED resource defined above to the platform so we
-    # can reference it below.
     platform.add_resources(oled_pmod)
 
     platform.build(ST7789Test(), do_program=True)
